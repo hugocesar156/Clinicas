a083d8540c10dd2b5b0fa7eba6df83b4ee50374b
Hugo César <hugocesar802@gamil.com>
2021-08-25 19:27:17 -0300

    - Criado model para gerenciamento de criação de cripotografia de senha de usuário.

---
+++ Clinicas/Models/Shared/Criptografia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Clinicas.Models.Shared
+{
+    public class Criptografia
+    {
+        public static string TratarCriptografia(string senha)
+        {
+            try
+            {
+                var salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+                return BCrypt.Net.BCrypt.HashPassword(senha, salt);
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static bool ValidarCriptografia(string senha, string hash)
+        {
+            try
+            {
+                return BCrypt.Net.BCrypt.Verify(senha, hash);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
